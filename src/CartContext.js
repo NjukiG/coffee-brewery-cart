90218949532a4fcf0d6d0cd4bf82803238d91a79
George <gnjuki19@gmail.com>
2023-05-31 17:59:21 +0300

    In the context component defining the function to be able to add quantity, remove, delete, get total cost

--- src/CartContext.js
+++ src/CartContext.js
@@ -1,5 +1,79 @@
 import { createContext, useState } from "react";
 import { productsArray } from "./productsStore";
 
+export const CartContext = createContext({
+  items: [],
+  getProductQuantity: () => {},
+  addOneToCart: () => {},
+  removeOneFromCart: () => {},
+  deleteFromCart: () => {},
+  getTotalCost: () => {},
+});
 
-const CartContext = createContext()
+export function CartProvider({ children }) {
+  const [cartProducts, setCartProducts] = useState([]);
+
+  const getProductQuantity = (id) => {
+    const quantity = cartProducts.find(
+      (product) => product.id === id
+    )?.quantity;
+    if (quantity === undefined) {
+      return 0;
+    }
+    return quantity;
+  };
+
+  const addOneToCart = (id) => {
+    const quantity = getProductQuantity(id);
+
+    if (quantity === 0) {
+      setCartProducts([...cartProducts, { id: id, quantity: 1 }]);
+    } else {
+      setCartProducts(
+        cartProducts.map((product) =>
+          product.id === id
+            ? { ...product, quantity: product.quantity + 1 }
+            : product
+        )
+      );
+    }
+  };
+
+  const removeOneFromCart = (id) => {
+    const quantity = getProductQuantity();
+
+    if (quantity == 1) {
+      deleteFromCart(id);
+    } else {
+      setCartProducts(
+        cartProducts.map((product) =>
+          product.id === id
+            ? { ...product, quantity: product.quantity - 1 }
+            : product
+        )
+      );
+    }
+  };
+
+  const deleteFromCart = (id) => {
+    setCartProducts((cartProducts) =>
+      cartProducts.filter((currentProduct) => currentProduct.id != id)
+    );
+  };
+
+  const getTotalCost = ()=> {
+      let totalCost = 0
+  }
+
+  const contextValue = {
+    items: cartProducts,
+    getProductQuantity,
+    addOneToCart,
+    removeOneFromCart,
+    deleteFromCart,
+    getTotalCost,
+  };
+  return (
+    <CartContext.Provider value={contextValue}>{children}</CartContext.Provider>
+  );
+}
